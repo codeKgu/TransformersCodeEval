4f1e6b354fb9661c5010affed1ee84091ec05586
Ken <ken.qgu@gmail.com>
2021-12-09 17:12:34 -0800

    delete results

--- PythonProgrammingPuzzles/results/09-12-2021_17-02-00/run.sh
+++
@@ -1,6 +0,0 @@
-conda activate TransformerCodeComparison_GPU
-python src/run_programming_puzzles_experiments.py \
-	-n=30 \
-	--engine=EleutherAI/gpt-neo-2.7B \
-	--filename=397puzzles.json \
-	--output_folder=results
